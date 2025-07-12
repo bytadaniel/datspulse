ad746c83f7e10dd8fb18f306eeac210f8f469c01
bytadaniel <danielbyta.work@gmail.com>
2025-07-12 13:59:05 +0300

    Implement base playable case

--- matchmaker/src/feature/move/index.ts
+++ matchmaker/src/feature/move/index.ts
@@ -1,13 +1,7 @@
-import { Ant, PlayerResponse } from "../../api/dto/player-response";
-import { getArena } from "../../api/methods/get-arena";
-import { tryMove } from "../../api/methods/move";
+import { AntMoveCommand } from "../../api/dto/player-move-commands";
+import { Ant, Hex, PlayerResponse } from "../../api/dto/player-response";
 
 export class AutoMoveClass {
-  private roundDuration: number = 10 * 60 * 1000; // 10 минут в миллисекундах
-  private turnDuration: number = 2 * 1000; // 2 секунды в миллисекундах
-  private isRunning: boolean = false;
-  constructor() {}
-
   private randomDirection(q: number, r: number): { q: number; r: number } {
     const neighbours = [
       { q: q, r: r - 1 }, // Северо-запад
@@ -20,34 +14,43 @@ export class AutoMoveClass {
     return neighbours[Math.floor(Math.random() * neighbours.length)];
   }
 
-  private generateMoves(ants: Ant[]): Ant[] {
-    return ants.map((ant) => {
-      const nextPosition = this.randomDirection(ant.q, ant.r);
-      ant.q = nextPosition.q;
-      ant.r = nextPosition.r;
-      return ant;
-    });
+  private generatePath(state: PlayerResponse, ant: Ant, maxDepth = 10): Hex[] {
+    let depth = 0;
+    depth++;
+    if (depth > maxDepth) {
+      return []; // стоим на месте
+    }
+
+    const next = this.randomDirection(ant.q, ant.r);
+    if (state.home.some((c) => c.q === next.q && c.r === next.r)) {
+      return this.generatePath(state, ant);
+    }
+
+    if (state.enemies.some((c) => c.q === next.q && c.r === next.r)) {
+      return this.generatePath(state, ant);
+    }
+
+    if (state.food.some((c) => c.q === next.q && c.r === next.r)) {
+      return this.generatePath(state, ant);
+    }
+
+    if (
+      state.ants.some(
+        (c) => c.q === next.q && c.r === next.r && ant.type === c.type
+      )
+    ) {
+      return this.generatePath(state, ant);
+    }
+
+    return [next];
   }
 
-  async runAutoMove() {
-    try {
-      const arena = await getArena();
-      const antsAfterMove = this.generateMoves((arena as PlayerResponse).ants);
-      const antsMoveRequest = antsAfterMove.map((antAfterMove) => ({
-        ant: antAfterMove.id,
-        path: [
-          {
-            q: antAfterMove.q,
-            r: antAfterMove.r,
-          },
-        ],
-      }));
-      await tryMove({ moves: antsMoveRequest });
-    } catch (error) {
-      console.error(
-        "Произошла ошибка во время выполнения авто-движения:",
-        error
-      );
-    }
+  runAutoMove(state: PlayerResponse, ants: Ant[]): AntMoveCommand[] {
+    return ants.map((ant) => {
+      return {
+        ant: ant.id,
+        path: this.generatePath(state, ant),
+      };
+    });
   }
 }
